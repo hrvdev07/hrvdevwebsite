18fb79f1b22f3ab8613abb27adf760c5c49511b9
Harris Dev <136090286+hrvdev07@users.noreply.github.com>
2024-06-13 02:20:59 +0700

    Add files via upload

---
+++ assset/scripts.js
@@ -0,0 +1,14 @@
+function showToastWithIcon(message, iconClass) {
+    var toast = document.getElementById('toast');
+    var toastIcon = document.getElementById('toastIcon');
+    var toastMessage = document.getElementById('toastMessage');
+    
+    toastIcon.innerHTML = '<i class="' + iconClass + '"></i>';
+    toastMessage.innerHTML = message;
+    toast.style.display = 'block';
+    
+    setTimeout(function() {
+      toast.style.display = 'none';
+    }, 2000); // Hiển thị toast trong 2 giây
+}
+  
