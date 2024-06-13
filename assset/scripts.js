55f94eb4b2ec7a98ebebb40ec7b6f2ac492f16c4
Harris Dev <136090286+hrvdev07@users.noreply.github.com>
2024-06-13 06:51:00 +0000

    reedit

--- assset/scripts.js
+++ assset/scripts.js
@@ -1,14 +1,14 @@
+// scripts.js
 function showToastWithIcon(message, iconClass) {
-    var toast = document.getElementById('toast');
-    var toastIcon = document.getElementById('toastIcon');
-    var toastMessage = document.getElementById('toastMessage');
-    
-    toastIcon.innerHTML = '<i class="' + iconClass + '"></i>';
-    toastMessage.innerHTML = message;
-    toast.style.display = 'block';
-    
-    setTimeout(function() {
-      toast.style.display = 'none';
-    }, 5000); // Hiển thị toast trong 5 giây
-}
+  var toast = document.getElementById('toast');
+  var toastIcon = document.getElementById('toastIcon');
+  var toastMessage = document.getElementById('toastMessage');
   
+  toastIcon.innerHTML = '<i class="' + iconClass + '"></i>';
+  toastMessage.innerHTML = message;
+  toast.className = "toast show";
+  
+  setTimeout(function() {
+      toast.className = toast.className.replace("show", "");
+  }, 5000); // Hiển thị toast trong 5 giây
+}
